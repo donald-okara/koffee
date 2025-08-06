da2f0007d9fbc832294ee0f9eba64187ea892097
donald-okara <donaldokara123@gmail.com>
2025-08-06 11:12:20 +0300

    Remove Maven Publish and signing configurations
    The `maven-publish` and `signing` plugins, along with their associated configurations (group, version, Maven publishing details, POM information, and signing), have been removed from the `koffee/build.gradle.kts` file.
    
    The Android namespace has also been updated from `io.github.donald-okara` to `ke.don.koffee`.

--- koffee/build.gradle.kts
+++ koffee/build.gradle.kts
@@ -1,20 +1,13 @@
-import com.vanniktech.maven.publish.SonatypeHost
-
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     id("org.jetbrains.dokka") version "1.9.20"
-    id("com.vanniktech.maven.publish") version "0.30.0"
-    id("maven-publish")
-    id("signing")
 }
 
-group = "io.github.donald-okara"
-version = System.getenv("GITHUB_REF_NAME")?.removePrefix("v") ?: "unspecified"
 
 android {
-    namespace = "io.github.donald-okara"
+    namespace = "ke.don.koffee"
     compileSdk = 36
 
     defaultConfig {
@@ -66,36 +59,3 @@ dependencies {
 }
 
 
-mavenPublishing {
-    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
-
-    signAllPublications()
-
-    coordinates("io.github.donald-okara", "koffee", version.toString())
-
-    pom {
-        name = "Koffee"
-        description = "A composable toast/snackbar framework for Android"
-        inceptionYear = "2025"
-        url = "https://github.com/donald-okara/Koffee"
-        licenses {
-            license {
-                name = "The Apache License, Version 2.0"
-                url = "https://www.apache.org/licenses/LICENSE-2.0.txt"
-                distribution = "https://www.apache.org/licenses/LICENSE-2.0.txt"
-            }
-        }
-        developers {
-            developer {
-                id = "donald-okara"
-                name = "Donald Okara"
-                url = "https://github.com/donald-okara"
-            }
-        }
-        scm {
-            url = "https://github.com/donald-okara/Koffee"
-            connection = "scm:git:git://github.com/donald-okara/Koffee.git"
-            developerConnection = "scm:git:ssh://git@github.com:donald-okara/Koffee.git"
-        }
-    }
-}

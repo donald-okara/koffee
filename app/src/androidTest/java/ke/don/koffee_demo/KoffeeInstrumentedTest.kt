7b5fc057a91d7c58c69fad9380e8ed2bee900f5c
donald-okara <donaldokara123@gmail.com>
2025-08-07 17:06:32 +0300

    fix: update instrumented tests and workflow
    This commit updates the instrumented tests and the corresponding GitHub workflow.
    
    The main changes include:
    - Renaming the instrumented test file and updating its package.
    - Modifying the GitHub workflow to build and test the `app` module instead of the `koffee` module.
    - Updating the paths for the app and test APKs in the workflow.

--- app/src/androidTest/java/ke/don/koffee_demo/KoffeeInstrumentedTest.kt
+++ app/src/androidTest/java/ke/don/koffee_demo/KoffeeInstrumentedTest.kt
@@ -1,19 +1,4 @@
-/*
- * Copyright © 2025 Donald O. Isoe (isoedonald@gmail.com)
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-package ke.don.koffee
+package ke.don.koffee_demo
 
 import androidx.activity.ComponentActivity
 import androidx.compose.material3.Button
@@ -27,7 +12,7 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import ke.don.koffee.domain.Koffee
 import ke.don.koffee.model.ToastAction
 import ke.don.koffee.ui.DefaultToast
-import org.junit.Assert.assertTrue
+import org.junit.Assert
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -113,7 +98,7 @@ class KoffeeInstrumentedTest {
         composeTestRule.onNodeWithText("Confirm").performClick()
 
         // Verify result
-        assertTrue(clicked)
+        Assert.assertTrue(clicked)
     }
 
     /**
@@ -152,7 +137,7 @@ class KoffeeInstrumentedTest {
         composeTestRule.onNodeWithText("Confirm").performClick()
 
         // Verify result
-        assertTrue(clicked)
+        Assert.assertTrue(clicked)
     }
 
     /**

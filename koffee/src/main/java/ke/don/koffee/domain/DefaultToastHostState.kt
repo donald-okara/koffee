c0df5292abebe7e801815b4470a94ca9cd342261
donald-okara <donaldokara123@gmail.com>
2025-08-12 16:32:24 +0300

    style: run spotless apply

--- koffee/src/main/java/ke/don/koffee/domain/DefaultToastHostState.kt
+++ koffee/src/main/java/ke/don/koffee/domain/DefaultToastHostState.kt
@@ -13,7 +13,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
 import androidx.lifecycle.compose.LocalLifecycleOwner
@@ -148,8 +147,6 @@ internal class DefaultToastHostState internal constructor(
         _toasts.clear()
     }
 }
-
-
 
 /**
  * Remembers and provides a [ToastHostState] instance, scoped to the current composition
